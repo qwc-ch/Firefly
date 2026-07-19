7bc5d8a4ef0ad144dc5a1b95c0fc6af7f89b4c57
年华 <zzzzzzxx2022@163.com>
2026-07-19 20:46:10 +0800

    Update friends config via admin

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -14,7 +14,7 @@ export const friendsPageConfig: FriendsPageConfig = {
 // 友链配置
 export const friendsConfig: FriendLink[] = [
 	{
-	title: "夏夜流萤(测试)",
+	title: "夏夜流萤",
 	imgurl: "https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
 	desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
 	siteurl: "https://blog.cuteleaf.cn",
