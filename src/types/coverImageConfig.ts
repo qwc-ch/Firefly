5f0b37757f1b3fcf158612c900b306de2b4e66e4
qwc <zzzzzzxx2022@163.com>
2026-07-27 17:25:13 +0800

    新增文章并合并上游

--- src/types/coverImageConfig.ts
+++ src/types/coverImageConfig.ts
@@ -5,5 +5,7 @@ export type CoverImageConfig = {
 	randomCoverImage: {
 		enable: boolean; // 是否启用随机图功能
 		apis: string[]; // 随机图API列表
+		fallback?: string; // API失败时的回退图片路径
+		showLoading?: boolean; // 是否显示加载动画
 	};
 };
