a1ebd65908a871b1e8deb9668cd0cb1a53bcae71
年华 <zzzzzzxx2022@163.com>
2026-08-19 11:48:48 +0800

    Update friends config via admin

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -146,8 +146,7 @@ export const friendsConfig: FriendLink[] = [
 	},
 	{
 		title: "L!!!!ght",
-		imgurl:
-			"https://easyimg.kejk.cn/i/4484873c-c2cc-4b3d-bc35-5c72ed01cfd9.webp",
+		imgurl: "https://easyimg.kejk.cn/i/4484873c-c2cc-4b3d-bc35-5c72ed01cfd9.webp",
 		desc: "阳光正好，慢慢前行。",
 		siteurl: "https://sunlight.kejk.cn",
 		rss: "https://sunlight.kejk.cn/rss.xml",
@@ -328,8 +327,7 @@ export const friendsConfig: FriendLink[] = [
 	},
 	{
 		title: "xane",
-		imgurl:
-			"https://cloudflare-imgbed-d88.pages.dev/file/1784102742642_头像.jpg",
+		imgurl: "https://cloudflare-imgbed-d88.pages.dev/file/1784102742642_头像.jpg",
 		desc: "Keep going.",
 		siteurl: "https://xane.eu.cc",
 		rss: "https://xane.eu.cc/rss.xml",
@@ -486,6 +484,16 @@ export const friendsConfig: FriendLink[] = [
 		rss: "https://baiyangmu.xyz/rss.xml",
 		tags: ["Blog"],
 		weight: 11, // 权重，数字越大排序越靠前
+		enabled: true, // 是否启用
+	},
+	{
+		title: "Phantomxjc",
+		imgurl: "https://avatars.githubusercontent.com/phantomxjc?v=4&s=640",
+		desc: "记录个人生活和学习的一个网站。",
+		siteurl: "https://xjc.ccwu.cc",
+		rss: "https://xjc.ccwu.cc/rss.xml",
+		tags: ["Blog"],
+		weight: 10, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
 	},
 ];
