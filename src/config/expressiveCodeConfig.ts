a09838768b2d4cabb85e349e9096c702894ad00a
qwc-ch <zzzzzzxx2022@163.com>
2026-08-20 12:22:38 +0800

    全屏透明适配及修改配置文件

--- src/config/expressiveCodeConfig.ts
+++ src/config/expressiveCodeConfig.ts
@@ -11,7 +11,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	darkTheme: "github-dark-default",
 
 	// 亮色主题（用于亮色模式）
-	lightTheme: "github-dark-default",
+	lightTheme: "snazzy-light",
 
 	// 更多样式请看expressive-code的官方文档
 	// https://expressive-code.com/guides/themes/
